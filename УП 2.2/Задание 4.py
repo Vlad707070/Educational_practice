5077566cbbcb7cb0af58c5284edef0d502a3af35
Your Name <vsamodelov89@gmail.com>
2025-04-15 22:14:52 +0700

    solving the fourth task

---
+++ УП 2.2/Задание 4.py
@@ -0,0 +1,27 @@
+class Counter:
+    def __init__(self, count):
+        self.count = count
+
+    def shows_the_current_number(self):
+        print(f"{self.count} - начальное число")
+
+    def increases_by_one(self):
+        self.count += 1
+        print(f"{self.count} - новое число, увеличенное на один")
+
+def checks_the_number():
+    while True:
+        user_input = input("Введите число(начальное значение счетчика). В противном случае значение по умолчанию будет 0: ")
+        if not user_input:
+            print("Начальное значение взято как по умолчанию и равно 0")
+            return 0
+
+        try:
+            return int(user_input)
+        except ValueError:
+            print(f"вы ввели {type(user_input)}, а надо ввести число. Попробуйте еще раз")
+
+value = checks_the_number()
+counter = Counter(value)
+counter.shows_the_current_number()
+counter.increases_by_one()
