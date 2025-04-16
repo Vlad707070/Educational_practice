969e3e4100a5c6a016ae6a8d3f483e0f7268fcfb
Your Name <vsamodelov89@gmail.com>
2025-04-16 10:04:33 +0700

    editing

--- УП 2.2/Задание 5.py
+++ УП 2.2/Задание 5.py
@@ -1,4 +1,4 @@
-class Example:
+class Examples:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
@@ -11,11 +11,11 @@ class Example:
         print(f"Объект с x = {self.x}, y = {self.y} удалён")
 
 print("Создание объекта с заданными значениями:")
-obj1 = Example(5, 10)
+obj1 = Examples(5, 10)
 obj1.show()
 
 print("\nСоздание объекта со значениями по умолчанию:")
-obj2 = Example()  # x=0, y=0
+obj2 = Examples()  # x=0, y=0
 obj2.show()
 
 print("\nУдаление объектов:")
